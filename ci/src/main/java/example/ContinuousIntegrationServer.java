3ecb58d391069c345465ed15fe2c74875aed74d8
Filip Hedman <fhedma@kth.se>
2025-02-10 16:11:48 +0100

    Closes issue #3 feat: add code to setup server on port 8080

--- ci/src/main/java/example/ContinuousIntegrationServer.java
+++ ci/src/main/java/example/ContinuousIntegrationServer.java
@@ -5,9 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+
 
 public class ContinuousIntegrationServer {
-    public static void main(String[] ){
-        
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+
+        // make the ServerConnector component listen on ALL network interfaces
+        ServerConnector connector = new ServerConnector(server);
+        // port
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+
+        // DEBUGG
+        System.out.println("starting CI server on ALL interfaces (0.0.0.0:8080)");
+
+        server.start();
+        server.join();
     }
 }
